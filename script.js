9dea476def59604303d3d3dd1df2fa2e39503193
IGOR-GAB30 <igor_paradella@estudante.sesisenai.org.br>
2026-03-18 09:02:55 -0300

    ADD- Adicionei as funcionalidades/Valores aos botões da calculadora usando o onclick no html e sua função respectiva pelo javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+//inserir valores aos botões da calculadora
+function insertValor(valor) {
+    document.getElementById('resultado').value += valor;
+}
+
+//apricar o resultado da conta com o =
+function showresult() {
+    const resultado = eval(document.getElementById('resultado').value);
+    document.getElementById('resultado').value = resultado;
+}
+
+//limpar elementos da conta
+function limpaValores() {
+    document.getElementById('resultado').value = '';
+}
